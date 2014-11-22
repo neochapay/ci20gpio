a594873db5068e296ac9d721ba556a195621a503
Chupligin Sergey <neochapay@gmail.com>
2014-11-22 16:42:18 +0300

    Next steep

---
+++ src/ci20_gpio.c
@@ -0,0 +1,33 @@
+#include "ci20_gpio.h"
+
+static uint8_t debug = 0;
+
+void ci20_setDebug(uint8_t d)
+{
+    debug = d;
+}
+
+void ci20_initPin(uint8_t pin)
+{
+
+}
+
+void ci20_setMode(uint8_t pin, uint8_t mode)
+{
+
+}
+
+void ci20_setValue(uint8_t pin, uint8_t value)
+{
+
+}
+
+unit8_t ci20_getMode(uint8_t pin)
+{
+    return 0;
+}
+
+uint8_t ci20_getValue(uint8_t pin)
+{
+    return 0;
+}
